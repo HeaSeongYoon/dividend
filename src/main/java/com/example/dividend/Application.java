4c4e4ba8400ebe277c4213f1f37d9a09c41eeb0a
HeaSeongYoon <yoonhs989898@gmail.com>
2023-08-03 19:46:10 +0900

    배당금 저장 완료

--- src/main/java/com/example/dividend/Application.java
+++ src/main/java/com/example/dividend/Application.java
@@ -1,6 +1,9 @@
 package com.example.dividend;
 
 
+import com.example.dividend.model.Company;
+import com.example.dividend.scraper.Scraper;
+import com.example.dividend.scraper.YahooFinanceScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +13,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+
+        Scraper scraper = new YahooFinanceScraper();
+//        var result =  scraper.scrap(Company.builder().ticker("0").build());
+        var result = scraper.scrapCompanyByTicker("MMM");
+        System.out.println(result);
 
     }
 
