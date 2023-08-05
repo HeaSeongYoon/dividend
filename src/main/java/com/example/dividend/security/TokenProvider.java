320da2303fe9c2249eda0d2f67f4dd3a658fddcc
HeaSeongYoon <yoonhs989898@gmail.com>
2023-08-06 00:26:25 +0900

    토큰을 사용한 인증 구현

--- src/main/java/com/example/dividend/security/TokenProvider.java
+++ src/main/java/com/example/dividend/security/TokenProvider.java
@@ -1,11 +1,15 @@
 package com.example.dividend.security;
 
+import com.example.dividend.service.MemberService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -19,8 +23,11 @@ public class TokenProvider {
     private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
     private static final String KEY_ROLES = "roles";
 
-    @Value("{spring.jwt.secret}")
+    private final MemberService memberService;
+
+    @Value("${spring.jwt.secret}")
     private String secretKey;
+
 
 
     /**
@@ -44,6 +51,11 @@ public class TokenProvider {
                 .signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
                 .compact();
 
+    }
+
+    public Authentication getAuthentication(String jwt) {
+        UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUsername(String token) {
